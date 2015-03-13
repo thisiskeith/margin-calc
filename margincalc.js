77c327cb2acdc59aa59c66d3d117802bce85a50a
Keith Hansen <designsbykeith@gmail.com>
2015-03-13 15:01:01 -0700

    added base object

---
+++ margincalc.js
@@ -0,0 +1,35 @@
+(function (undefined) {
+
+    'use strict';
+
+    var margincalc = {};
+
+    margincalc.grossProfit = function (revenue, cost) {
+        return revenue - cost;
+    }
+
+    margincalc.grossMarginPercentage = function (revenue, cost, precision) {
+
+        precision = precision || 2;
+
+        var p = this.grossProfit(revenue, cost);
+        var g = parseFloat((p / revenue * 100).toFixed(precision));
+
+        return g;
+    };
+
+    margincalc.markUpPercentage = function (revenue, cost, precision) {
+
+        precision = precision || 2;
+
+        var p = this.grossProfit(revenue, cost);
+        var m = parseFloat(((p / cost) * 100).toFixed(precision));
+
+        return m;
+    };
+
+    module.exports = margincalc;
+
+}).call(this);
+
+
