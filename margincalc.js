de8c950527ae0b3c0534adf3bda21407223bb422
Thomas Nicolosi <tnicolosi@Thomass-MacBook-Pro.local>
2015-07-20 11:20:40 -0700

    Added validation and error handling. Added a basic .gitignore file.

--- margincalc.js
+++ margincalc.js
@@ -2,12 +2,68 @@
 
 var margincalc = {};
 
+function validate(obj) {
+    // returns validation result object
+    var result = {
+        valid: true,
+        message: ''
+    };
+
+    // throws immediately if obj input is not correct
+    if (!obj) {
+        throw 'margin-calc exception: No validation data received.';
+    }
+    if (typeof obj !== 'object') {
+        throw 'margin-calc exception: Validation input must be an object.';
+    }
+
+    // grossMargin must be < 100%
+    if (obj.grossMargin && parseFloat(obj.grossMargin) >= 100) {
+        result.valid = false;
+        result.message += 'margin-calc exception: Gross margin entered must be less than 100%. ';
+    }
+    // revenue and cost must be > 0
+    if (obj.cost && parseFloat(obj.cost) <= 0) {
+        result.valid = false;
+        result.message += 'margin-calc exception: Cost entered must be greater than 0. ';
+    }
+    if (obj.revenue && parseFloat(obj.revenue) <= 0) {
+        result.valid = false;
+        result.message += 'margin-calc exception: Revenue entered must be greater than 0.';
+    }
+
+    return result;
+}
+
 margincalc.grossProfit = function (revenue, cost) {
+    try {
+        var testData = validate({
+            revenue: revenue,
+            cost: cost
+        });
+    } catch (e) {
+        console.log(e);
+    }
+
+    if (!testData.valid && testData.message) {
+        throw testData.message;
+    }
     return revenue - cost;
 };
 
 margincalc.grossMarginPercentage = function (revenue, cost, precision) {
+    try {
+        var testData = validate({
+            revenue: revenue,
+            cost: cost
+        });
+    } catch (e) {
+        console.log(e);
+    }
 
+    if (!testData.valid && testData.message) {
+        throw testData.message;
+    }
     precision = precision || 2;
 
     var p = this.grossProfit(revenue, cost);
@@ -17,6 +73,18 @@ margincalc.grossMarginPercentage = function (revenue, cost, precision) {
 };
 
 margincalc.markUpPercentage = function (revenue, cost, precision) {
+    try {
+        var testData = validate({
+            revenue: revenue,
+            cost: cost
+        });
+    } catch (e) {
+        console.log(e);
+    }
+
+    if (!testData.valid && testData.message) {
+        throw testData.message;
+    }
 
     precision = precision || 2;
 
@@ -26,11 +94,23 @@ margincalc.markUpPercentage = function (revenue, cost, precision) {
     return m;
 };
 
-margincalc.revenueFromGmPercentage = function (gmPercentage, cost, precision) {
+margincalc.revenueFromGrossMarginPercentage = function (gmPercentage, cost, precision) {
+    try {
+        var testData = validate({
+            grossMargin: gmPercentage,
+            cost: cost
+        });
+    } catch (e) {
+        console.log(e);
+    }
+
+    if (!testData.valid && testData.message) {
+        throw testData.message;
+    }
 
     precision = precision || 2;
 
-    var revenue = parseFloat(((100 * cost)/(100 - gmPercentage)).toFixed(precision));
+    var revenue = parseFloat(((100 * cost) / (100 - gmPercentage)).toFixed(precision));
 
     return revenue;
 };
