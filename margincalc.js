e1d18d2b52dd81e963a78deaf96b58697ef97e42
Thomas Nicolosi <tnicolosi@Thomass-MacBook-Pro.local>
2015-07-22 14:01:31 -0700

    Updated error handling to properly capture undefined, null and NaN cases.

--- margincalc.js
+++ margincalc.js
@@ -10,22 +10,44 @@ margincalc.validate = function (obj) {
     // throws immediately if obj input is not correct
     if (!obj || typeof obj !== 'object') {
         throw new Error(EX_PREFIX + 'Validation input must be an object.');
-        return false;
     }
 
-    // grossMargin must be < 100%
-    if (obj.grossMargin && parseFloat(obj.grossMargin) >= 100) {
-        throw new Error(EX_PREFIX + 'Gross margin entered must be less than 100%.');
-        return false;
+    // cost must be passed and must not be undefined, null, or NaN
+    if (obj.hasOwnProperty('cost') &&
+        (typeof obj.cost === 'undefined' ||
+            obj.cost === null ||
+            obj.cost.length <= 0 ||
+            obj.cost != obj.cost)) {
+        throw new Error(EX_PREFIX + 'Cost must be a number.');
     }
-    // revenue and cost must be > 0
-    if (obj.cost && parseFloat(obj.cost) <= 0) {
+    // cost must be > 0
+    if (obj.hasOwnProperty('cost') && parseFloat(obj.cost) <= 0) {
         throw new Error(EX_PREFIX + 'Cost entered must be greater than 0.');
-        return false;
     }
-    if (obj.revenue && parseFloat(obj.revenue) <= 0) {
+
+    // grossMargin must not be undefined, null, or NaN
+    if (obj.hasOwnProperty('grossMargin') &&
+        (typeof obj.grossMargin === 'undefined' ||
+            obj.grossMargin === null ||
+            obj.grossMargin.length <= 0 ||
+            obj.grossMargin != obj.grossMargin)) {
+        throw new Error(EX_PREFIX + 'Gross margin must be a number.');
+    }
+    // grossMargin must be < 100%
+    if (obj.hasOwnProperty('grossMargin') && parseFloat(obj.grossMargin) >= 100) {
+        throw new Error(EX_PREFIX + 'Gross margin entered must be less than 100%.');
+    }
+    // revenue must not be undefined, null, or NaN
+    if (obj.hasOwnProperty('revenue') &&
+        (typeof obj.revenue === 'undefined' ||
+            obj.revenue === null ||
+            obj.revenue.length <= 0 ||
+            obj.revenue != obj.revenue)) {
+        throw new Error(EX_PREFIX + 'Revenue must be a number.');
+    }
+    // revenue must be > 0
+    if (obj.hasOwnProperty('revenue') && parseFloat(obj.revenue) <= 0) {
         throw new Error(EX_PREFIX + 'Revenue entered must be greater than 0.');
-        return false;
     }
 
     return true;
