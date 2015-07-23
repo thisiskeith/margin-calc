1d2ebef187e9541b699aa97e9756ed5d9aca10ad
Thomas Nicolosi <tnicolosi@Thomass-MacBook-Pro.local>
2015-07-22 17:50:12 -0700

    Revised validation to put hasOwnProperty call  in a loop.

--- margincalc.js
+++ margincalc.js
@@ -12,42 +12,33 @@ margincalc.validate = function (obj) {
         throw new Error(EX_PREFIX + 'Validation input must be an object.');
     }
 
-    // cost must be passed and must not be undefined, null, or NaN
-    if (obj.hasOwnProperty('cost') &&
-        (typeof obj.cost === 'undefined' ||
-            obj.cost === null ||
-            obj.cost.length <= 0 ||
-            obj.cost != obj.cost)) {
-        throw new Error(EX_PREFIX + 'Cost must be a number.');
-    }
-    // cost must be > 0
-    if (obj.hasOwnProperty('cost') && parseFloat(obj.cost) <= 0) {
-        throw new Error(EX_PREFIX + 'Cost entered must be greater than 0.');
-    }
+    var config = [
+        {name: 'cost', rule: 1, label: 'Cost'},
+        {name: 'revenue', rule: 1, label: 'Revenue'},
+        {name: 'grossMargin', rule: 2, label: 'Gross margin'}];
 
-    // grossMargin must not be undefined, null, or NaN
-    if (obj.hasOwnProperty('grossMargin') &&
-        (typeof obj.grossMargin === 'undefined' ||
-            obj.grossMargin === null ||
-            obj.grossMargin.length <= 0 ||
-            obj.grossMargin != obj.grossMargin)) {
-        throw new Error(EX_PREFIX + 'Gross margin must be a number.');
-    }
-    // grossMargin must be < 100%
-    if (obj.hasOwnProperty('grossMargin') && parseFloat(obj.grossMargin) >= 100) {
-        throw new Error(EX_PREFIX + 'Gross margin entered must be less than 100%.');
-    }
-    // revenue must not be undefined, null, or NaN
-    if (obj.hasOwnProperty('revenue') &&
-        (typeof obj.revenue === 'undefined' ||
-            obj.revenue === null ||
-            obj.revenue.length <= 0 ||
-            obj.revenue != obj.revenue)) {
-        throw new Error(EX_PREFIX + 'Revenue must be a number.');
-    }
-    // revenue must be > 0
-    if (obj.hasOwnProperty('revenue') && parseFloat(obj.revenue) <= 0) {
-        throw new Error(EX_PREFIX + 'Revenue entered must be greater than 0.');
+    for (var i in config) {
+        if (obj.hasOwnProperty(config[i].name)) {
+            // property passed and must not be undefined, null, or NaN
+            if (typeof obj[config[i].name] === 'undefined' ||
+                 obj[config[i].name] === null ||
+                 obj[config[i].name].length <= 0 ||
+                 obj[config[i].name] != obj[config[i].name]) {
+                throw new Error(EX_PREFIX + config[i].label + ' must be a number.');
+            }
+            if (config[i].rule === 1) {
+                // cost or revenue must be > 0
+                if (parseFloat(obj[config[i].name]) <= 0) {
+                    throw new Error(EX_PREFIX + config[i].label + ' entered must be greater than 0.');
+                }
+            }
+            if (config[i].rule === 2) {
+                // gross margin must be < 100%
+                if (parseFloat(obj[config[i].name]) >= 100) {
+                    throw new Error(EX_PREFIX + config[i].label + ' entered must be less than 100%.');
+                }
+            }
+        }
     }
 
     return true;
